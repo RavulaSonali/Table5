0cff033665f75ecf14346ecd79ea03ce59b5f715
Ravula Sonali <ravula.sonali@gmail.com>
2024-01-10 12:05:12 +0530

    Insert

--- obj/Debug/net6.0/ConAppTable5.AssemblyInfo.cs
+++ obj/Debug/net6.0/ConAppTable5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConAppTable5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc732885c3b34a40fdaa64dc09f0ab57703204b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConAppTable5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConAppTable5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
